835bf7941eef1cdc095eac7448b71f37e079c02a
rcdavis <rcdavisgd@gmail.com>
2023-08-06 07:29:38 -0400

    Updated Delete material screen

--- app/src/main/java/mhw/inventory/materials/DeleteMaterialScreen.kt
+++ app/src/main/java/mhw/inventory/materials/DeleteMaterialScreen.kt
@@ -1,6 +1,7 @@
 package mhw.inventory.materials
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -8,7 +9,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import mhw.inventory.BaseDialog
 import mhw.inventory.R
+import mhw.inventory.TextInputField
 import mhw.inventory.ui.theme.MHWBoardGameInventoryTheme
 import mhw.inventory.utils.getMockMaterials
 
@@ -18,27 +21,13 @@ fun DeleteMaterialScreen(
     onCancel: (() -> Unit)? = null,
     onConfirm: (Material) -> Unit
 ) {
-    AlertDialog(
-        title = {
-            Text(stringResource(R.string.delete_material_title))
-        },
-        text = {
-            Text(stringResource(R.string.delete_material_text, material.name))
-        },
-        onDismissRequest = onCancel ?: {},
-        confirmButton = {
-            TextButton(onClick = { onConfirm(material) }) {
-                Text(stringResource(android.R.string.ok))
-            }
-        },
-        dismissButton = {
-            onCancel?.let {
-                TextButton(onClick = it) {
-                    Text(stringResource(android.R.string.cancel))
-                }
-            }
-        }
-    )
+    BaseDialog(
+        titleTextId = R.string.delete_material_title,
+        onConfirm = { onConfirm(material) },
+        onCancel = onCancel
+    ) {
+        Text(stringResource(R.string.delete_material_text, material.name))
+    }
 }
 
 @Preview(name = "Light Mode")
